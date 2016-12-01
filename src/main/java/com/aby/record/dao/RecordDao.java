072423a84e513ee73878596e22a28f7b76c9623a
Abyetc <abyjun@gmail.com>
2016-12-01 16:58:59 +0800

    Change sqlSessionFactory to singleTon mode

--- src/main/java/com/aby/record/dao/RecordDao.java
+++ src/main/java/com/aby/record/dao/RecordDao.java
@@ -1,7 +1,9 @@
 package com.aby.record.dao;
 
+import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -10,24 +12,58 @@ import org.junit.Test;
 import com.aby.record.model.Record;
 
 public class RecordDao {
+
+	private static SqlSessionFactory sqlSessionFactory;
+
+	public RecordDao(){
+		sqlSessionFactory = getSqlSessionFactory();
+	}
+	
+	
+	// 使用单例模式管理sqlSessionFactory
+	public static SqlSessionFactory getSqlSessionFactory() {
+		if (sqlSessionFactory == null) {
+			synchronized (RecordDao.class) {
+				if (sqlSessionFactory == null) {
+					InputStream inputStream = null;
+					try {
+						inputStream = Resources.getResourceAsStream("mybatis.xml");
+						sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+					} catch (IOException e) {
+						throw new RuntimeException(e.getCause());
+					} finally {
+						try {
+							inputStream.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+		return sqlSessionFactory;
+	}
+
+	public static SqlSession openSession() {
+		return getSqlSessionFactory().openSession();
+	}
+
 	@Test
 	public void testGetStudent() throws Exception {
-		// 1. 加载MyBatis的配置文件：mybatis.xml（它也加载关联的映射文件，也就是mappers结点下的映射文件）
-		InputStream in = this.getClass().getClassLoader().getResourceAsStream("mybatis.xml");
-		// 2. SqlSessionFactoryBuidler实例将通过输入流调用build方法来构建 SqlSession 工厂
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
-		// 3. 通过工厂获取 SqlSession 实例，SqlSession 完全包含了面向数据库执行 SQL 命令所需的所有方法。
-		SqlSession session = sqlSessionFactory.openSession();
-		// 4. 准备基本信息
-		
+		Record record = getRecordByID(341545);
+		if (record == null) {
+			System.out.println("Null");
+		}else {
+			System.out.println(record.getPromName());
+		}
+	}
+	
+	public static Record getRecordByID(int id){
+		SqlSession session = RecordDao.getSqlSessionFactory().openSession();
 		RecordMapper mapper = session.getMapper(RecordMapper.class);
-		Record record = mapper.selectByPrimaryKey(341545);
-//		System.out.println(record);
-//		System.out.println(record.getTotalAmt());
-//		System.out.println(record.getTotalRentalAmt());
-		System.out.println(record.getPromName());
-		// 6. 关闭输入流和SqlSession实例
-		in.close();
+		Record record = mapper.selectByPrimaryKey(id);
 		session.close();
+		return record;
 	}
 }
